9339405b91080fd8fb6ab95d30cc09691cbd36bb
bleeptrack <mail@bleeptrack.de>
2025-07-13 12:16:16 +0200

    animation rotation and style

--- static/AnimationPresenter.mjs
+++ static/AnimationPresenter.mjs
@@ -8,29 +8,27 @@ export class AnimationPresenter extends HTMLElement {
 		// creating the inner HTML of the editable list element
 		container.innerHTML = `
 			<style>
+                 @import url('/static/style.css');
 				canvas {
-					width: 100%;
-					height: 100%;
+					width: 100% !important;
+					height: 100% !important;
 				}
                 #author {
                     position: absolute;
                     bottom: 0;
-                    left: 0;
+                    right: 2vh;
                     width: 100%;
                     height: 100px;
-                    background-color: rgba(0, 0, 0, 0.5);
-                    color: white;
-                    padding: 10px;
-                    font-size: 12px;
-                    text-align: center;
                 }
 			</style>
 			
-			<div id="author"></div>
+			<h1 id="author"></h1>
 			<canvas id="canvas"></canvas>
 		`;
 
 		this.shadow.appendChild(container.content.cloneNode(true));
+        this.loadedSnippets = [];
+        this.currentPointer = 0;
 
 	}
 
@@ -48,15 +46,29 @@ export class AnimationPresenter extends HTMLElement {
     }
 
     runAnimation() {
+        paper.project.clear();
         this.loadSnippets().then(snippets => {
             console.log(snippets);
-            const newestSnippet = snippets.sort((a, b) => new Date(b.updated) - new Date(a.updated))[0];
-            console.log('Newest snippet:', newestSnippet);
 
-            this.loadSnippet(newestSnippet.name).then(code => {
-                console.log(code);
+            let allSnippets = snippets.sort((a, b) => new Date(b.updated) - new Date(a.updated)).slice(0, 10);
+            for(let snippet of allSnippets) {
+                if(this.loadedSnippets.find(s => s.name === snippet.name)) {
+                    console.log("skipping", snippet.name);
+                    continue;
+                }
+                this.loadedSnippets.unshift(snippet);
+                if(this.loadedSnippets.length > 10) {
+                    this.loadedSnippets.pop();
+                }
+                this.currentPointer = this.loadedSnippets.length - 1;
+            }
+
+            console.log("loadedSnippets", this.loadedSnippets.map(snippet => snippet.name));
+            this.loadSnippet(this.loadedSnippets[this.currentPointer].name).then(code => {
+                console.log("pointer", this.currentPointer);
                 paper.PaperScript.execute(code, paper);
-                this.shadow.getElementById('author').innerHTML = newestSnippet.name.split('.')[0];
+                this.shadow.getElementById('author').innerHTML = this.loadedSnippets[this.currentPointer].name.split('.')[0];
+                this.currentPointer = (this.currentPointer + 1) % this.loadedSnippets.length;
             });
         });
     };
